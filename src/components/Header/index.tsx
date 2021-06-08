2276ffa54eb84f87ec072a969a443bac0ea093c8
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-06-08 20:42:17 -0300

    types and component summary

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
-import { Container } from './styles';
+import logoIcon from "../../assets/logo.svg";
+
+import { HeaderProps } from "../../types/types";
+
+import { Container, Content } from "./styles";
 
 export function Header() {
   return (
     <Container>
-      
+      <Content>
+        <img src={logoIcon} alt="dtmoney" />
+
+        <button >Nova Transação</button>
+      </Content>
     </Container>
-  )
+  );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,35 @@
 import { Container } from './styles';
 
+import incomeIcon from "../../assets/income.svg";
+import outcomeIcon from "../../assets/outcome.svg";
+import totalIcon from "../../assets/total.svg";
+
 export function Summary() {
   return (
-    <Container></Container>
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeIcon} alt="Entradas" />
+        </header>
+          <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeIcon} alt="Saidas" />
+        </header>
+          <strong>- R$500,00</strong>
+      </div>
+      
+      <div>
+        <header>
+          <p>Total</p>
+          <img src={totalIcon} alt="Toral" />
+        </header>
+          <strong>R$500,00</strong>
+      </div>
+    </Container>
   )
 }

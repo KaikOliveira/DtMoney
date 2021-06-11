6f633ebe659ea3aea31f210649d06b816b48ebbe
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-06-11 19:56:46 -0300

    finishapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
-import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
+
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+
+import Modal from "react-modal";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+
+import { TransactionsProvider } from "./hooks/useTransactions";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
-    <>
+    <TransactionsProvider>
       <Header />
+
       <Dashboard />
+
       <NewTransactionModal
-        isOpen={true}
-        onRequestClose={() => {console.log('safa');
-        }}
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
       />
+
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
